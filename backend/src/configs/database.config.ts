5b0a5c4a0198847b7bbd477ed5175ad53996eb22
Priyanshu-2024 <priyanshu.patel@iflair.com>
2024-03-18 19:03:55 +0530

    e2

--- backend/src/configs/database.config.ts
+++ backend/src/configs/database.config.ts
@@ -1,13 +1,13 @@
-import {connect , ConnectOptions} from 'mongoose';
+// import {connect , ConnectOptions} from 'mongoose';
 
 
-export const dbConnect = () =>{
-    connect(process.env.MONGODB_URI!,{
-        // useNewUrlParser:true,
-        // useUnifiedRopology:true
-    } as ConnectOptions).then(()=>{
-        console.log('connect Successfully'),
-        (error: any) => console.log(error)
+// export const dbConnect = () =>{
+//     connect(process.env.MONGODB_URI!,{
+//         // useNewUrlParser:true,
+//         // useUnifiedRopology:true
+//     } as ConnectOptions).then(()=>{
+//         console.log('connect Successfully'),
+//         (error: any) => console.log(error)
         
-    }
-)}
+//     }
+// )}
