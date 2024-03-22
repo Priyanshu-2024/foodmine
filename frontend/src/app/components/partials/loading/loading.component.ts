81707e1b5a1c5cecca1dc84f59c881358d761357
Priyanshu-2024 <priyanshu.patel@iflair.com>
2024-03-22 16:39:15 +0530

    patch update

--- frontend/src/app/components/partials/loading/loading.component.ts
+++ frontend/src/app/components/partials/loading/loading.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { LoadingService } from 'src/app/services/loading.service';
 
 @Component({
@@ -6,16 +6,19 @@ import { LoadingService } from 'src/app/services/loading.service';
   templateUrl: './loading.component.html',
   styleUrls: ['./loading.component.css']
 })
-export class LoadingComponent {
+export class LoadingComponent implements OnInit{
   isLoading!: boolean;
   constructor(loadingService: LoadingService) {
     loadingService.isLoading.subscribe((isLoading) => {
       this.isLoading = isLoading;
     });
+    loadingService.showLoading()
+ 
 
 
    }
 
   ngOnInit(): void {
   }
+
 }
